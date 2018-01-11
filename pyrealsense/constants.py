9a4ea0db8be2031e8ae4c161cd9f827d39a9e952
Pablo Prietz <pablo@prietz.org>
2018-01-11 18:58:18 +0100

    setup: Use package_data instead of data_files setup-arg for consistent lrs_parsed_classes installation

--- pyrealsense/constants.py
+++ pyrealsense/constants.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Licensed under the Apache-2.0 License, see LICENSE for details.
 
-"""This module extract the RS_API_VERSION to which pyrealsense is binded and wraps several enums 
+"""This module extract the RS_API_VERSION to which pyrealsense is binded and wraps several enums
 from rs.h into classes with the same name."""
 
 import pycparser
@@ -14,14 +14,11 @@ import os
 import sys
 from os import environ, path
 
-
-
-
 loaded_globals = {}
 loaded_classes = {}
 
 if getattr(sys, 'frozen', False):
-    res_path = os.path.join(sys._MEIPASS,'pyrealsense')
+    res_path = os.path.join(sys._MEIPASS, 'pyrealsense')
 
 else:
     res_path = os.path.dirname(__file__)
@@ -31,13 +28,11 @@ try:
         loaded_classes_list = pickle.load(ser2_classes)
         lrs_globals, lrs_classes = loaded_classes_list
         for name, val in lrs_globals.items():
-            globals()[name] = val 
-
+            globals()[name] = val
 
         for class_name, class_dict in lrs_classes.items():
             class_gen = type(class_name, (object,), class_dict)
-            #print("class dict:", class_dict);
+            # print("class dict:", class_dict);
             globals()[class_name] = class_gen
 except IOError:
     raise
-
